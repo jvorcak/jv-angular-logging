7e6cba28789f9a5681d27706eb05c1e80178d715
Ján Vorčák <vorcak.jan@gmail.com>
2014-09-20 17:16:58 +0200

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name jvLoggingApp
+ * @description
+ * # jvLoggingApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('jvLoggingApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jvLoggingApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jvLoggingApp
+ */
+angular.module('jvLoggingApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
